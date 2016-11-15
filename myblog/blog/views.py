55ea40855dfe7970d4c74653f4197cd9e6bf8fda
borrascador <roflbot101@gmail.com>
2016-11-15 12:44:02 -0800

    Added posts and post template

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, DetailView
+from django.utils import timezone
+from .models import Post
 
 class HomeView(TemplateView):
-    template_name = "index.html"
+    template_name = "post_list.html"
+
+    def posts(self):
+        return Post.objects.all()
